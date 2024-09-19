d6ea04c649e1bab388b7a35d825dae680cd3e556
jonatan-c <jona.cc.93@gmail.com>
2024-09-19 19:07:01 -0300

    fix: .env not reading 2

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,15 +1,13 @@
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const url = process.env.NEXT_PUBLIC_BASE_URL;
-
   return [
     {
-      url: `${url}`,
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
       lastModified: new Date(),
     },
     {
-      url: `${url}/information`,
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/information`,
       lastModified: new Date(),
     },
   ];
